7efda7043ce0c9aaf1df15722b385cc77caf2759
VINAY KUMAR RAPAKA <107675897+Vinayrapaka1@users.noreply.github.com>
2022-07-07 20:16:10 +0530

    Add files via upload

---
+++ list_methods_part1.py
@@ -0,0 +1,71 @@
+# a=[]
+# print(a,type(a))
+
+# a=[3,4]
+# print(a,type(a))
+
+# b=['warangal',17,3.2]
+# print(b,type(b))
+# print(b[0],b[1],b[2])
+# print(b[0],type(b[0]))
+# print(b[1],type(b[1]))
+# print(b[2],type(b[2]))
+
+# a=[15,2.7,'kazipet',None,True]
+# print(a[0],type(a[0]))
+# print(a[1],type(a[1]))
+# print(a[2],type(a[2]))
+# print(a[3],type(a[3]))
+# print(a[4],type(a[4]))
+# print(a[5],type(a[5]))
+
+# b='hi warangal'
+# b[0]='H'
+
+# v=[5,6.6,'hanamkonda']
+# v[0]=7
+# v[2]='kazipet'
+# print(v)
+
+# c=[3,6.3,'hello',[24,45]]
+# print(c,type(c))
+# print(c[0],type(c[0]))
+# print(c[1],type(c[1]))
+# print(c[2],type(c[2]))
+# print(c[3],type(c[3]))
+# print(c[3][0],type(c[3][0]))
+# print(c[3][1],type(c[3][1]))
+
+# b=[1,3.4,'hello nani',[12,34,'nani']]
+# b[0]=7
+# b[3][2]='vinnu'
+# print(b)
+
+# v=[3,6.3,'hyderabad',[11,34]]
+# v.append('vinay')
+# print(v)
+
+# a=[2,3.4,'hello',[45,56]]
+# a.clear()
+# print(a) # only square brackets will come []
+
+# b=[23,2.3,'hello',10,23]
+# print(b.count('hello'))
+# print(b.count(23))
+# print(b.count(2.3))
+# print(b.count(10))
+
+# a=[3,4,5,6]
+# b=[10,11,12,13]
+# # a.extend(b)
+# # print(a) #[3,4,5,6,10,11,12,13]
+# b.extend(a)
+# print(b)  #[10,11,12,13,3,4,5,6]
+
+# n=[1,3,4,1,8,4]
+# print(n.count(1))
+
+# a=[1,2,4,5,'hi']
+# print(a.index(4))
+# print(a.index(1))
+# print(a.index(5))
